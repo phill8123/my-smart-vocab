fb750e4542d93b7fa17509a27a31b80312e89952
phill8123 <kjh93386@gmail.com>
2025-12-24 00:31:10 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,15 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
   const env = loadEnv(mode, '.', '');
 
   return {
     plugins: [react()],
     define: {
-      'process.env.API_KEY': JSON.stringify(env.API_KEY)
+      // Vercel의 System Environment Variable(process.env.API_KEY)을 우선적으로 사용하고,
+      // 로컬 개발 환경에서는 .env 파일의 값(env.API_KEY)을 사용합니다.
+      'process.env.API_KEY': JSON.stringify(process.env.API_KEY || env.API_KEY)
     }
   };
 });
